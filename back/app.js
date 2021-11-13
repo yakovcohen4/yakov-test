03df7703da99d74eff1b973d0ca6310fe73d1fb0
yakov cohen <89575070+yakovcohen4@users.noreply.github.com>
2021-11-13 16:36:55 +0200

    Initial commit

---
+++ back/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const userRouter = require('./routers/user');
+const errorHandlingMiddleware = require('./middlewares/errorHandlingMiddleware');
+const unknownEndpoint = require('./middlewares/unknownEndpoint');
+
+const port = process.env.PORT || 3000;
+
+const mongo = process.env.DATABASE;
+
+mongoose //connect
+  .connect(mongo)
+  .then((result) => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+//heroku
+
+app.use('/', express.static(`./front/dist/`));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/front/dist/index.html');
+});
+
+app.get('/', (req, res) => {
+  res.send('working');
+});
+app.use('/user', userRouter);
+
+// unknownEndpoint handling middleware
+app.use(unknownEndpoint);
+
+// error handling middleware
+app.use(errorHandlingMiddleware);
+
+app.listen(port, () => {
+  console.log(`litsening in port ${port}...`);
+});
